d6046880715a55086a0855db979454e5438929a7
kpete2017 <60599938+kpete2017@users.noreply.github.com>
2020-05-27 10:50:51 -0600

    Menu
    Worked strucutre for menu

--- lib/hangman_game.rb
+++ lib/hangman_game.rb
@@ -1,54 +1,63 @@
 class HangmanGame
-# ASCII Created by Chris Horton
-# https://gist.github.com/chrishorton/8510732aa9a80a03c829b09f12e20d9c
-@hangman_array = ['''
-    +---+
-    |   |
-        |
-        |
-        |
-        |
-  =========''', '''
-    +---+
-    |   |
-    O   |
-        |
-        |
-        |
-  =========''', '''
-    +---+
-    |   |
-    O   |
-    |   |
-        |
-        |
-  =========''', '''
-    +---+
-    |   |
-    O   |
-   /|   |
-        |
-        |
-  =========''', '''
-    +---+
-    |   |
-    O   |
-   /|\  |
-        |
-        |
-  =========''', '''
-    +---+
-    |   |
-    O   |
-   /|\  |
-   /    |
-        |
-  =========''', '''
-    +---+
-    |   |
-    O   |
-   /|\  |
-   / \  |
-        |
-  =========''']
+  def initialize
+    system 'clear'
+    build_hangman
+    puts "made it"
+  end
+
+  def build_hangman
+        # ASCII Created by Chris Horton
+    # https://gist.github.com/chrishorton/8510732aa9a80a03c829b09f12e20d9c
+    @hangman_array = ['''
+      +---+
+      |   |
+          |
+          |
+          |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+          |
+          |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+      |   |
+          |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+     /|   |
+          |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+     /|\  |
+          |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+     /|\  |
+     /    |
+          |
+    =========''', '''
+      +---+
+      |   |
+      O   |
+     /|\  |
+     / \  |
+          |
+    =========''']
+    puts @hangman_array
+  end
 end
--- lib/menu.rb
+++ lib/menu.rb
@@ -1,9 +1,38 @@
+require 'pry'
 class MainMenu
     def initialize
+        system "clear"
         prompt = TTY::Prompt.new
         banner = File.read "bannerfile.txt"
         puts banner.colorize :light_blue
-        prompt.select("  MAIN MENU ", %w(New_Game New_Player Leaderboard EXIT))
 
+
+        choice = prompt.select("  MAIN MENU") do |menu|
+            menu.choice 'New Game'
+            menu.choice 'New Player'
+            menu.choice 'Leaderboard'
+            menu.choice 'EXIT'
+        end
+
+ 
+        if choice == 'New Game'
+            HangmanGame.new
+        end
+        if choice == "New Player"
+            intro
+        end
+        if choice == "Leaderboard"
+            "Future Leaderboard"
+        end
+        if choice == "EXIT"
+            system 'clear'
+        end
+    end    
+
+    def intro
+        puts "Welcome to Hangman! What should your wanted poster read?"
+        name_input = gets.chomp
+        self.player = Player.new(name_input)
+        puts "Howdy #{self.player.name}!"
     end
 end
